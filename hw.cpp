e506b8ded54da8ad70050b067f3e4cdeba31741d
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-10-23 09:16:30 -0300

    finiched chapter 4

---
+++ hw.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // includes the iostream header file content into our source file via the #include directive
+
+int main(){
+    // comment
+    /*multi-line
+    comment*/
+    std::cout << "Hello World! :D"; // std::cout object, also known as the standard output stream.
+                                    // The << operator, called the stream insertion operator
+    std::cout << "\nFirst " << "Second";
+}
